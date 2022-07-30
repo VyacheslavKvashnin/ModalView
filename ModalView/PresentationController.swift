38cb2ebebb3fd8576ed084aa5511ef7b5bbe34a0
komplektan <komplektan@mail.ru>
2022-07-30 23:47:25 +0500

    Finish project

---
+++ ModalView/PresentationController.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationController.swift
+//  ModalView
+//
+//  Created by Вячеслав Квашнин on 30.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class PresentationController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
--- ModalView/ViewController.swift
+++ ModalView/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let buttonPresentationController: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Presentation Controller", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        buttonPresentationController.frame = CGRect(x: 0, y: 0, width: 300, height: 50)
+        view.addSubview(buttonPresentationController)
+        
+        buttonPresentationController.center = view.center
+    }
+    
+    @objc
+    func tapButton() {
+        let presentationVC = PresentationController()
+        presentationVC.modalTransitionStyle = .coverVertical
+        presentationVC.modalPresentationStyle = .formSheet
+        present(presentationVC, animated: true)
     }
 }
 
